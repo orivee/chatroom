e01feb96910ae36c5844fb3af5a48e22bf776185
koneko <libesse@koneko>
2019-08-30 15:02:40 +0800

    实现在一个客户端上「接受服务端的消息」或「向服务端发送消息」

--- client.c
+++ client.c
@@ -17,7 +17,7 @@ char * s_gets(char * st, int n)
         while (st[i] != '\n' && st[i] != '\0')
             i++;
         if (st[i] == '\n')
-            st[i] == '\0';
+            st[i] = '\0';
         else
             while (getchar() != '\n')
                 continue;
@@ -76,7 +76,7 @@ protocol_t * provide_message()
     eatinput();
     puts("enter your messgae");
     s_gets(message.message, MSGMAX);
-    /* printf("DEBUG mesage: %s\n", message.message); */
+    printf("DEBUG mesage: %s\n", message.message);
 
     memcpy(pprot->datap, &message, sizeof(message_t));
 
@@ -98,19 +98,114 @@ void write_to_server(int fd, protocol_t * pprot)
     free(pprot);
 }
 
+int read_from_client(int cfd)
+{
+    int retval;
+    protocol_t * pprot;
+
+    pprot = malloc(sizeof(protocol_t) + sizeof(message_t));
+    memset(pprot, 0x00, sizeof(protocol_t) + sizeof(message_t));
+
+    printf("DEBUG read()\n");
+    retval = read(cfd, pprot, sizeof(protocol_t));
+    if (retval <= 0)
+    {
+        if (0 == retval)
+        {
+            perror("read()");
+            /* 写端断开，应该从 epoll 中移除此 fd */
+
+            return 0;
+        }
+        else
+        {
+            perror("read()");
+            return -1;
+        }
+    }
+    else
+    {
+        printf("message size: %d\n", pprot->size);
+    }
+
+    message_t * pmsg = (message_t *) malloc(sizeof(message_t));
+
+    retval = read(cfd, pmsg, sizeof(message_t));
+    if (retval <= 0)
+    {
+        if (0 == retval)
+        {
+            perror("read()");
+            free(pmsg);
+            return 0;
+        }
+        else
+        {
+            perror("read()");
+            free(pmsg);
+            return -1;
+        }
+    }
+    else
+    {
+        printf("target: %d, message: %s\n", pmsg->fd, pmsg->message);
+        /* free(pmsg); */
+    }
+
+    return 1;
+}
+
 int 
 main(int argc, char *argv[])
 {
-    int cfd;
+    int cfd, epfd;
+    struct epoll_event ev, events[20];
+    int nfound;
     protocol_t * pprotocol;
 
     client_sock_init(&cfd);
+    epfd = epoll_create(20);
+    perror("epoll_create()");
+
+    ev.events = EPOLLIN;
+    ev.data.fd = cfd;
+    epoll_ctl(epfd, EPOLL_CTL_ADD, cfd, &ev);
+    perror("epoll_ctl()");
+
+    /* 把 stdin 加入到 epoll */
+    ev.events = EPOLLIN;
+    ev.data.fd = 0;
+    epoll_ctl(epfd, EPOLL_CTL_ADD, 0, &ev);
+
+    printf("DEBUG read()\n");
 
     while (1)
     {
-        pprotocol = provide_message();
-        write_to_server(cfd, pprotocol);
+        printf("DEBUG epoll_wait() ...\n");
+        nfound = epoll_wait(epfd, events, 20, -1);
+        perror("epoll_wait()");
+
+        for (int i = 0; i < nfound; i++)
+        {
+            // Check inputs of client
+            if (0 == events[i].data.fd)
+            {
+                pprotocol = provide_message();
+                write_to_server(cfd, pprotocol);
+            }
+            else // Check received from server
+            {
+                read_from_client(events[i].data.fd);
+            }
+        }
     }
+
+    /* while (1) */
+    /* { */
+    /*     pprotocol = provide_message(); */
+    /*     write_to_server(cfd, pprotocol); */
+    /*     read_from_server(cfd, pprotocol); */
+    /* } */
 
     return 0;
 }
