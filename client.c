1f3d3356fbac98ea605a9a5d4dd7302d458cebef
koneko <libesse@koneko>
2019-09-03 13:34:07 +0800

    客户端在读取失败后退出

--- client.c
+++ client.c
@@ -29,138 +29,6 @@ char * s_gets(char * st, int n)
     return retval;
 }
 
-#if 0
-/* description: initiliaze client socket */
-/* precondition: poi*pnt to a socket fd */
-/* condition: client socket fd connects peer socket */
-void client_sock_init(int * pcfd)
-{
-    int retval;
-    struct sockaddr_in saddr;
-
-    *pcfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (-1 == *pcfd)
-    {
-        /* TODO: log */
-        perror("socket()");
-        exit(-1);
-    }
-
-    saddr.sin_family = AF_INET;
-    saddr.sin_port = htons(30000);
-    if (inet_pton(AF_INET, "127.0.0.1", &saddr.sin_addr) <= 0)
-    {
-        /* TODO log */
-        fprintf(stderr, "fail to convert IPv4 address!\n");
-        exit(-1);
-    }
-
-    retval = connect(*pcfd, (struct sockaddr *) &saddr, sizeof(saddr));
-    if (-1 == retval)
-    {
-        /* TODO log */
-        perror("connect()");
-        exit(-1);
-    }
-
-}
-
-/* description: 准备要发送的消息 */
-/* postcondtion: */
-protocol_t * provide_message()
-{
-    message_t message;
-    protocol_t * pprot = NULL;
-
-    pprot = malloc(sizeof(protocol_t) + sizeof(message_t));
-    memset(pprot, 0, sizeof(protocol_t) + sizeof(message_t));
-    pprot->size = sizeof(message_t);
-
-    puts("enter a client fd");
-    scanf("%d", &message.fd);
-    eatinput();
-    puts("enter your messgae");
-    s_gets(message.message, MSGMAX);
-    printf("DEBUG mesage: %s\n", message.message);
-
-    memcpy(pprot->datap, &message, sizeof(message_t));
-
-    return pprot;
-}
-
-/* description: 发送准备好的消息 */
-void write_to_server(int fd, protocol_t * pprot)
-{
-    int retval;
-
-    printf("DEBUG write() ...\n");
-    retval = write(fd, pprot, sizeof(protocol_t) + sizeof(message_t));
-    if (-1 == retval)
-    {
-        /* TODO: log */
-        perror("write()");
-    }
-    free(pprot);
-}
-
-int read_from_client(int cfd)
-{
-    int retval;
-    protocol_t * pprot;
-
-    pprot = malloc(sizeof(protocol_t) + sizeof(message_t));
-    memset(pprot, 0x00, sizeof(protocol_t) + sizeof(message_t));
-
-    printf("DEBUG read()\n");
-    retval = read(cfd, pprot, sizeof(protocol_t));
-    if (retval <= 0)
-    {
-        if (0 == retval)
-        {
-            perror("read()");
-            /* 写端断开，应该从 epoll 中移除此 fd */
-
-            return 0;
-        }
-        else
-        {
-            perror("read()");
-            return -1;
-        }
-    }
-    else
-    {
-        printf("message size: %d\n", pprot->size);
-    }
-
-    message_t * pmsg = (message_t *) malloc(sizeof(message_t));
-
-    retval = read(cfd, pmsg, sizeof(message_t));
-    if (retval <= 0)
-    {
-        if (0 == retval)
-        {
-            perror("read()");
-            free(pmsg);
-            return 0;
-        }
-        else
-        {
-            perror("read()");
-            free(pmsg);
-            return -1;
-        }
-    }
-    else
-    {
-        printf("target: %d, message: %s\n", pmsg->fd, pmsg->message);
-        /* free(pmsg); */
-    }
-
-    return 1;
-}
-#endif
-
 
 void setup_client_connect(int * pconnfd)
 {
@@ -202,27 +70,6 @@ void rdev_add(int epfd, int fd)
     }
 }
 
-msgprot_t * message_pack(char * msg)
-{
-    msgprot_t * pmsgprot = malloc(sizeof(msgprot_t) + strlen(msg));
-    pmsgprot->length = strlen(msg);
-    memcpy(pmsgprot->msgp, msg, strlen(msg));
-    return pmsgprot;
-}
-
-/* 返回消息长度 */
-int message_unpack(int connfd, char ** pmsg, size_t size)
-{
-        *pmsg = malloc(size + 1);
-        int rlen = read(connfd, *pmsg, size);
-        (*pmsg)[rlen] = '\0';
-        if (rlen <= 0)
-        {
-            perror("message reading failed");
-        }
-        return rlen;
-}
-
 
 void send_message_server(msgprot_t * pmsgprot, int connfd)
 {
@@ -234,17 +81,17 @@ void send_message_server(msgprot_t * pmsgprot, int connfd)
     }
 }
 
-void send_message_private()
-{
-
-}
-
 char * accept_message(int connfd)
 {
     msgprot_t msgprot;
     char * pmsg;
 
-    read(connfd, &msgprot, sizeof(msgprot_t));
+    if (read(connfd, &msgprot, sizeof(msgprot_t)) <= 0)
+    {
+        perror("reading from server failed");
+        close(connfd);
+        exit(EXIT_FAILURE);
+    }
     message_unpack(connfd, &pmsg, msgprot.length);
 
     return pmsg;
